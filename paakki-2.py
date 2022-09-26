8b9153db295ffbe73aafada1133074dfe0224b0f
ottotm <75931482+ottotm@users.noreply.github.com>
2022-09-26 21:43:19 +0300

    Add files via upload

---
+++ paakki-2.py
@@ -0,0 +1,12 @@
+n = int(input("Anna luku: "))
+
+if n > 0:
+    fact = 1
+    
+    for i in range(1,n+1):
+        fact = fact * i
+        
+    print ("Luvun",n,"kertoma on ",end="")
+    print (fact)
+else:
+    print("Kiitos ja moi!")
