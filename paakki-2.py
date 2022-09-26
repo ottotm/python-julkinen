a7fd6de4237468cc0858f0c6d473b85e1e520f6d
ottotm <75931482+ottotm@users.noreply.github.com>
2022-09-26 22:08:53 +0300

    Update paakki-2.py

--- paakki-2.py
+++ paakki-2.py
@@ -1,12 +1,14 @@
-n = int(input("Anna luku: "))
+while True:
+    n = int(input("Anna luku: "))
 
-if n > 0:
-    fact = 1
-    
-    for i in range(1,n+1):
-        fact = fact * i
+    if n >  0:
+        fact = 1
         
-    print ("Luvun",n,"kertoma on ",end="")
-    print (fact)
-else:
-    print("Kiitos ja moi!")
+        for i in range(1,n+1):
+            fact = fact * i
+            
+        print ("Luvun",n,"kertoma on ",end="")
+        print (fact)
+    else:
+        print("Kiitos ja moi!")
+        break
